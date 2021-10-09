b2203e47395f43ae7a5dd3c132f3e89780ec2217
AlvaroSanchezDomingo <alvaro.sanchez.domingo@gmail.com>
2021-10-09 18:09:43 +0200

    First commit in develop branch

--- app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
+++ app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
@@ -9,3 +9,4 @@ data class PlacemarkModel(  var id: Long = 0,
                             var title: String = "",
                             var description: String = "",
                             var image: Uri = Uri.EMPTY) : Parcelable
+//comment
